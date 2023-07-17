88145360c6841bc0ed133c31128755f0a26885f2
Gayeon-Leee <123913974+Gayeon-Leee@users.noreply.github.com>
2023-07-18 01:22:08 +0900

    웹 크롤링 테스트

---
+++ Webtest/Webtest1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using HtmlAgilityPack;
+using System.Diagnostics;
+
+namespace Webtest1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 크롤링할 웹 사이트 URL 설정
+            string url = "https://www.weather.go.kr/w/obs-climate/land/city-obs.do";
+
+            // 웹 페이지 다운로드
+            string htmlContent = DownloadWebPage(url);
+
+            // HTML 파싱
+            ParseHtml(htmlContent);
+        }
+
+        static string DownloadWebPage(string url)
+        {
+            string htmlContent;
+            using (WebClient client = new WebClient())
+            {
+                // UTF-8로 인코딩된 웹 페이지 다운로드
+                client.Encoding = System.Text.Encoding.UTF8;
+                htmlContent = client.DownloadString(url);
+            }
+            return htmlContent;
+        }
+
+        static void ParseHtml(string htmlContent)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(htmlContent);
+
+            // 원하는 정보 추출을 위해 XPath를 사용하여 HTML 요소 선택
+            // 예를 들어, 모든 링크를 가져오려면:
+            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//a[@href]");
+
+            if (linkNodes != null)
+            {
+                foreach (HtmlNode linkNode in linkNodes)
+                {
+                    string linkUrl = linkNode.GetAttributeValue("href", "");
+                    Debug.Write(linkUrl);
+                }
+            }
+            else
+            {
+                Debug.Write("링크가 없습니다.");
+            }
+        }
+    }
+}
---
+++ Webtest/webtest2/Program.cs
@@ -0,0 +1,76 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+using System.Xml;
+
+namespace webtest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 크롤링할 웹 사이트 URL 설정
+            string url = "https://www.weather.go.kr/w/obs-climate/land/city-obs.do";
+
+            // 웹 페이지 다운로드
+            string htmlContent = DownloadWebPage(url);
+
+            // HTML 파싱하여 부산의 기온과 습도 추출
+            ParseWeatherData(htmlContent, "부산");
+        }
+
+        static string DownloadWebPage(string url)
+        {
+            string htmlContent;
+            using (WebClient client = new WebClient())
+            {
+                // UTF-8로 인코딩된 웹 페이지 다운로드
+                client.Encoding = System.Text.Encoding.UTF8;
+                htmlContent = client.DownloadString(url);
+            }
+            return htmlContent;
+        }
+
+        static void ParseWeatherData(string htmlContent, string city)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(htmlContent);
+
+            // 테이블의 모든 행을 가져옴
+            HtmlNodeCollection rows = doc.DocumentNode.SelectNodes("//table[@class='table-col']//tr");
+
+            if (rows != null)
+            {
+                bool foundCity = false;
+                foreach (HtmlNode row in rows)
+                {
+                    // 행에서 모든 셀을 가져옴
+                    HtmlNodeCollection cells = row.SelectNodes("td");
+
+                    if (cells != null && cells.Count >= 7)
+                    {
+                        string location = cells[0].InnerText.Trim();
+                        string temperature = cells[5].InnerText.Trim();
+                        string humidity = cells[6].InnerText.Trim();
+
+                        // 지역 이름이 부산이면 데이터를 출력하고 반복을 종료함
+                        if (location.Contains(city))
+                        {
+                            Console.WriteLine($"지역: {location}, 기온: {temperature}°C, 습도: {humidity}%");
+                            foundCity = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!foundCity)
+                {
+                    Console.WriteLine($"'{city}'의 데이터를 찾을 수 없습니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("데이터 테이블을 찾을 수 없습니다.");
+            }
+        }
+    }
+}
